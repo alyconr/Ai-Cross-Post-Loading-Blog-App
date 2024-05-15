79aadec14cf5352b3d2154dd7bc86e786b9568d4
alyconr <alyconr@hotmail.com>
2024-05-15 12:24:39 -0500

    :sparkles: Add controller to  handle cross posts articles

---
+++ Backend/controllers/devtoApi.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const { StatusCodes } = require("http-status-codes");
+const axios = require("axios");
+
+const postDevtoApi = async (req, res) => {
+  try {
+    const devToApiKey = "dds9t4CwnH7W9dz5hM29YDfM"; // Get your Dev.to API key
+    const devToEndpoint = "https://dev.to/api/articles";
+
+    const { title, body_markdown, published, tags } = req.body;
+
+    console.log("Received article data:", {
+      title,
+      body_markdown,
+      published,
+      tags,
+    });
+
+    const article = {
+      title,
+      body_markdown,
+      published,
+      tags,
+    };
+
+    const response = await axios.post(
+      devToEndpoint,
+      JSON.stringify({
+        article,
+      }),
+      {
+        headers: {
+          "api-key": devToApiKey,
+          accept: "application/vnd.forem.api-v1+json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    res.status(StatusCodes.OK).json(response.json);
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
+};
+
+module.exports = { postDevtoApi };

5452ee43def5590d7e286ed6a0fafeec9502a65b
alyconr <alyconr@hotmail.com>
2024-05-17 12:42:59 -0500

    :fire: Serilize and parse the tags array

--- Backend/controllers/draft.post.js
+++ Backend/controllers/draft.post.js
@@ -6,16 +6,18 @@ const pool = require("../db/connect");
 const getAllDraftPosts = async (req, res) => {
   const sql = "SELECT * FROM posts_draft";
 
-  const values = [];
-
-  pool.query(sql, values, (queryError, results) => {
+  pool.query(sql, [], (queryError, results) => {
     if (queryError) {
       console.error("Database query error:", queryError);
       res
         .status(StatusCodes.INTERNAL_SERVER_ERROR)
         .json({ error: "Database query error" });
     } else {
-      res.status(StatusCodes.OK).json({ posts: results });
+      const posts = results.map((post) => ({
+        ...post,
+        tags: JSON.parse(post.tags),
+      }));
+      res.status(StatusCodes.OK).json({ posts });
     }
   });
 };
@@ -38,7 +40,7 @@ const createDraftPost = async (req, res) => {
       .status(StatusCodes.UNAUTHORIZED)
       .json({ error: "Unauthorized invalid token" });
   }
-
+  const tagsString = JSON.stringify(req.body.tags);
   const values = [
     req.body.title,
     req.body.description,
@@ -47,10 +49,11 @@ const createDraftPost = async (req, res) => {
     req.body.date,
     decoded.id,
     req.body.category,
+    tagsString,
   ];
 
   const sql =
-    "INSERT INTO posts_draft(`title`, `description`, `content`,	 `image`, `date`,`userId`, `category` ) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    "INSERT INTO posts_draft(`title`, `description`, `content`,	 `image`, `date`,`userId`, `category`, `tags` ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
   pool.query(sql, values, (queryError, results) => {
     if (queryError) {
@@ -82,7 +85,7 @@ const updateDraftPost = async (req, res) => {
       .status(StatusCodes.UNAUTHORIZED)
       .json({ error: "Unauthorized invalid token" });
   }
-
+  const tagsString = JSON.stringify(req.body.tags);
   const values = [
     req.body.title,
     req.body.description,
@@ -91,11 +94,12 @@ const updateDraftPost = async (req, res) => {
     req.body.date,
     decoded.id,
     req.body.category,
+    tagsString,
     req.params.id,
   ];
 
   const sql =
-    "UPDATE posts_draft SET `title` = ?, `description` = ?, `content` = ?, `image` = ?, `date` = ?, `userId` = ?, `category` = ? ";
+    "UPDATE posts_draft SET `title` = ?, `description` = ?, `content` = ?, `image` = ?, `date` = ?, `userId` = ?, `category` = ?, `tags` = ? ";
 
   pool.query(sql, values, (queryError, results) => {
     if (queryError) {
@@ -110,9 +114,8 @@ const updateDraftPost = async (req, res) => {
 };
 
 const getSingleDraftPost = async (req, res) => {
-  const sql = "SELECT * FROM posts_draft WHERE id = ?";
-
   const values = [req.params.id];
+  const sql = "SELECT * FROM posts_draft ";
 
   pool.query(sql, values, (queryError, results) => {
     if (queryError) {
@@ -121,7 +124,15 @@ const getSingleDraftPost = async (req, res) => {
         .status(StatusCodes.INTERNAL_SERVER_ERROR)
         .json({ error: "Database query error" });
     } else {
-      res.status(StatusCodes.OK).json({ post: results[0] });
+      if (results.length > 0) {
+        const post = {
+          ...results[0],
+          tags: JSON.parse(results[0].tags),
+        };
+        res.status(StatusCodes.OK).json({ post });
+      } else {
+        res.status(StatusCodes.NOT_FOUND).json({ error: "Post not found" });
+      }
     }
   });
 };

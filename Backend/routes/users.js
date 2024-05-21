a515fcff2ff0f35ef9330d2e30ca29939b1ad751
alyconr <alyconr@hotmail.com>
2024-05-21 16:57:36 -0500

    :heavy_plus_sign: Add a  route to handle the devto api key token

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-const authorizedUser = require("../middleware/authorizedUser");
+const { authorizedUser, authorized } = require("../middleware/authorizedUser");
 
-const { getCurrentUser, updateUser, getUserPosts, getAllUsers, deleteUser } = require("../controllers/users");
+const {
+  getCurrentUser,
+  updateUser,
+  getUserPosts,
+  getAllUsers,
+  deleteUser,
+  updateDevToken,
+  getDevToken,
+} = require("../controllers/users");
 
-router.route("/:username").get(getCurrentUser).put(authorizedUser, updateUser).delete(deleteUser);
+router
+  .route("/:username")
+  .get(getCurrentUser)
+  .put(authorizedUser, updateUser)
+  .delete(deleteUser);
 router.route("/posts/:username").get(getUserPosts);
 router.route("/").get(getAllUsers);
+
+router
+  .route("/devToken/:userId")
+  .get(getDevToken)
+  .put(authorized, updateDevToken);
 
 module.exports = router;

0e79e8785bcefe58deeda57b7fcc71a56264d673
alyconr <alyconr@hotmail.com>
2024-05-23 10:56:31 -0500

    :fire: Remove styles and configure it on style components

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.css";
-
+import { styled } from "styled-components";
 
 function Hero() {
   const [index, setIndex] = useState(0);
@@ -11,45 +11,89 @@ function Hero() {
   };
 
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-        <img
-          style={{ height: "600px", width: "100%" }}
-          src="https://images.pexels.com/photos/1181325/pexels-photo-1181325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt="First slide"
-        />
-        <Carousel.Caption >
-          <h3 className="fw-bolder fs-1 text-white  ">Loading, The Technology Blog</h3>
-          <p>Where you can express your ideas </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          style={{ height: "600px", width: "100%" }}
-          src="https://images.pexels.com/photos/1261427/pexels-photo-1261427.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3 className="fw-bolder fs-1 text-white  ">Technology Stacks</h3>
-          <p>Web development, Cloud, Devops, Networking, Security and more</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          style={{ height: "600px", width: "100%" }}
-          src="https://images.pexels.com/photos/16018144/pexels-photo-16018144/free-photo-of-pessoa-programando-hacker.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2g"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3 className="fw-bolder fs-1 text-white  ">Social Networking</h3>
-          <p>
-           Share your Knowledge and learn on a daily basis.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <Wrapper>
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <img
+            src="https://images.pexels.com/photos/1181325/pexels-photo-1181325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="First slide"
+          />
+          <Carousel.Caption className="top-50">
+            <h3 className="fw-bolder fs-1 text-white  ">
+              Loading, The Technology Blog
+            </h3>
+            <p>Where you can express your ideas </p>
+            <div class="arrow bounce">
+              <a class="fa fa-arrow-down fa-2x" href="#Home"></a>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src="https://images.pexels.com/photos/1261427/pexels-photo-1261427.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="First slide"
+          />
+          <Carousel.Caption className="top-50">
+            <h3 className="fw-bolder fs-1 text-white  ">Technology Stacks</h3>
+            <p>Web development, Cloud, Devops, Networking, Security and more</p>
+            <div class="arrow bounce">
+              <a class="fa fa-arrow-down fa-2x" href="#Home"></a>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src="https://images.pexels.com/photos/16018144/pexels-photo-16018144/free-photo-of-pessoa-programando-hacker.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2g"
+            alt="First slide"
+          />
+          <Carousel.Caption className="top-50">
+            <h3 className="fw-bolder fs-1 text-white  ">Social Networking</h3>
+            <p>Share your Knowledge and learn on a daily basis.</p>
+            <div class="arrow bounce">
+              <a class="fa fa-arrow-down fa-2x" href="#Home"></a>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Wrapper>
   );
 }
 
 export default Hero;
 
+const Wrapper = styled.div`
+  .top-50 {
+    top: 50%;
+  }
+
+  img {
+    width: 100%;
+    height: 91vh;
+    object-fit: cover;
+  }
+  .arrow {
+    text-align: center;
+    margin: 8% 0;
+  }
+  .bounce {
+    -moz-animation: bounce 2s infinite;
+    -webkit-animation: bounce 2s infinite;
+    animation: bounce 2s infinite;
+  }
+
+  @keyframes bounce {
+    0%,
+    20%,
+    50%,
+    80%,
+    100% {
+      transform: translateY(0);
+    }
+    40% {
+      transform: translateY(-30px);
+    }
+    60% {
+      transform: translateY(-15px);
+    }
+  }
+`;

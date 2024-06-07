8b6142319248673ee278e903669a4c516b9d99c5
alyconr <alyconr@hotmail.com>
2024-06-07 15:34:53 -0500

    :sparkles: Add image slice

---
+++ Frontend/src/store/imageSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    fileUrl: localStorage.getItem("uploadedImage") || " ",
+};
+
+const imageSlice = createSlice({
+    name: "image",
+    initialState,
+    reducers: {
+        setFileUrl: (state, action) => {
+            state.fileUrl = action.payload;
+            localStorage.setItem("uploadedImage", action.payload);
+        },
+    },
+});
+
+export const { setFileUrl } = imageSlice.actions;
+export default imageSlice.reducer;

7783478787a592dc4832f07bb65240ece94c2b6e
alyconr <alyconr@hotmail.com>
2024-06-07 12:18:14 -0500

    :fire: Delete redux dependencies

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import {  AuthContextProvider } from "./context/authContext.jsx";
+import { AuthContextProvider } from "./context/authContext.jsx";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

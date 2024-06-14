4b45c04a6f4a2e0c22a3f5f199c9175e23bc9efa
alyconr <alyconr@hotmail.com>
2024-06-14 16:08:40 -0500

    :recycle: Refactor the utils function to recieve image metadata

--- Frontend/src/utils/devToApi.js
+++ Frontend/src/utils/devToApi.js
@@ -5,6 +5,7 @@ const handleCrossPostToDevTo = async (
   title,
   content,
   description,
+  image,
   category,
   tags,
   devToken,
@@ -12,6 +13,7 @@ const handleCrossPostToDevTo = async (
 ) => {
   setLoading(true);
 
+  console.log(image);
   const devToProxyEndPoint = "http://localhost:9000/api/v1/devto-proxy";
 
   const markdownContent = `# ${description}\n\n${content}`;
@@ -20,8 +22,9 @@ const handleCrossPostToDevTo = async (
     title: title,
     body_markdown: markdownContent,
     published: false,
+    main_image: `http://localhost:9000/uploads/${image?.metadata?.name}`,
     tags: [category, tags.toString()],
-    devToken
+    devToken,
   };
 
   try {

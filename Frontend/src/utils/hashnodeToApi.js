dfad7f379fdbec48df1a0cf08eec4e9e6ebd3b4d
alyconr <alyconr@hotmail.com>
2024-07-08 15:28:09 -0500

    :hammer: Validadte arrays in tags field

--- Frontend/src/utils/hashnodeToApi.js
+++ Frontend/src/utils/hashnodeToApi.js
@@ -7,12 +7,15 @@ const handleCrossPostToHashnode = async (
   description,
   tags,
   hashnodeToken,
-  hashnodePublicationId
+  hashnodePublicationId,
+  draftHashnode
 ) => {
   const hashnodeProxyEndPoint = "http://localhost:9000/api/v1/hashnode-proxy";
 
   const markdownContent = `# ${content}`;
-
+  
+ 
+  console.log(hashnodeToken)
   const articleData = {
     publicationId: hashnodePublicationId,
     title: title,
@@ -21,14 +24,15 @@ const handleCrossPostToHashnode = async (
     coverImageOptions: {
       coverImageURL:
         "https://caracoltv.brightspotcdn.com/dims4/default/13a675b/2147483647/strip/true/crop/1000x666+0+0/resize/1000x666!/format/webp/quality/90/?url=http%3A%2F%2Fcaracol-brightspot.s3.us-west-2.amazonaws.com%2Fcb%2F07%2Fd8719fa241a89a444d4a24e91d0d%2Fluis-diaz-liverpool-premier-league-west-ham.jpg",
-    },
-    tags: tags.map((tag) => ({
+    },    
+    tags: Array.isArray(tags) && tags.map((tag) => ({
       name: tag.toString(),
       slug: tag.toString().toLowerCase().replace(/ /g, "-"),
     })),
     hashnodeApiKey: hashnodeToken,
+    draft: draftHashnode ? true : false,
   };
-
+  console.log("Article data:", articleData);
   try {
     await axios.post(hashnodeProxyEndPoint, articleData, {
       headers: {

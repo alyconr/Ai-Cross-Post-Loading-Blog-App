ab7e9eb4bda1bf70687dc1cc0ae8f71d3d00a4a3
alyconr <alyconr@hotmail.com>
2024-07-16 11:02:48 -0500

    :wrench: Add a route to handle userId

--- Backend/routes/devtoApi.js
+++ Backend/routes/devtoApi.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { postDevtoApi } = require("../controllers/devtoApi");
+const { postDevtoApi, getPostsDevto } = require("../controllers/devtoApi");
 
 router.route("/").post(postDevtoApi);
+router.route("/:userId").get(getPostsDevto);
 
 module.exports = router;

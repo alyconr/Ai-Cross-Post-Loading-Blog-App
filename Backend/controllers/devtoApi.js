8dbc771549f4a6f0a9700830b065ee95ab41dfb0
alyconr <alyconr@hotmail.com>
2024-07-16 11:01:45 -0500

    :heavy_plus_sign:  Implement a  helper function to retrive devtoken from DB and use it in get posts controller

--- Backend/controllers/devtoApi.js
+++ Backend/controllers/devtoApi.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const { StatusCodes } = require("http-status-codes");
 const axios = require("axios");
+const pool = require("../db/connect");
 
 const postDevtoApi = async (req, res) => {
   try {
-    // Get your Dev.to API key
     const devToEndpoint = "https://dev.to/api/articles";
 
     const { title, body_markdown, published, main_image, tags, devToken } =
@@ -53,4 +53,48 @@ const postDevtoApi = async (req, res) => {
   }
 };
 
-module.exports = { postDevtoApi };
+const getPostsDevto = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const devToEndpoint = "https://dev.to/api/articles/me/published";
+    const devToToken = await getDevTokenFromDb(userId);
+
+    if (!devToToken) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: "Dev.to API key is required" });
+    }
+
+    const { data } = await axios.get(devToEndpoint, {
+      headers: {
+        "api-key": devToToken,
+        accept: "application/vnd.forem.api-v1+json",
+        "Content-Type": "application/json",
+      },
+    });
+    res.status(StatusCodes.OK).json(data);
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
+};
+
+const getDevTokenFromDb = async (userId) => {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT `devToToken` FROM users WHERE `id` = ?";
+    const values = [userId];
+
+    pool.query(sql, values, (queryError, results) => {
+      if (queryError) {
+        console.error("Database query error:", queryError);
+        reject(queryError);
+      } else {
+        resolve(results[0].devToToken);
+      }
+    });
+  });
+};
+
+module.exports = { postDevtoApi, getPostsDevto };

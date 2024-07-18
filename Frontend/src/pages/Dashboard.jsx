7bb61aee9e2a3c96f562d782b5df7f53bd0830ba
alyconr <alyconr@hotmail.com>
2024-07-18 16:42:43 -0500

    :sparkles: Introduce an active component state to keep track hich component shoul be displayed

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -6,8 +6,13 @@ import axios from "axios";
 import { useLocation } from "react-router-dom";
 import { AuthContext } from "../context/authContext";
 import useFetch from "../utils/useFetch";
+import Profile from "./Profile";
+import Settings from "../components/settings";
+import Bookmarks from "./Bookmarks";
+import Home from "./Home";
 const Dashboard = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [activeComponent, setActiveComponent] = useState("dashboard");
   const [followers, setFollowers] = useState([]);
   const [following, setFollowing] = useState([]);
   const [bookmarks, setBookmarks] = useState([]);
@@ -91,21 +96,44 @@ const Dashboard = () => {
 
     fetchBookmarks();
   }, [currentUser?.user.id]);
+
+  const renderContent = () => {
+    switch (activeComponent) {
+      case "profile":
+        return <Profile />;
+      case "localPosts":
+        return <Home />;      
+      case "readingList":
+        return <Bookmarks />;
+        case "settings":
+          return <Settings />;
+      case "dashboard":
+      default:
+        return (
+          <>
+            <h1>Dashboard</h1>
+            <CardsContainer>
+              <Card title="Followers" count={followers.length} />
+              <Card title="Followings" count={following.length} />
+              <Card title="Bookmarks" count={bookmarks.length} />
+              <Card title="Medium Posts" count={10} />
+              <Card title="Dev.to Posts" count={15} />
+              <Card title="Hashnode Posts" count={5} />
+              <Card title="Total Posts" count={30} />
+            </CardsContainer>
+          </>
+        );
+    }
+  };
+
   return (
     <DashboardContainer>
-      <Sidebar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <MainContent>
-        <h1>Dashboard</h1>
-        <CardsContainer>
-          <Card title="Followers" count={followers.length} />
-          <Card title="Followings" count={following.length} />
-          <Card title="Bookmarks" count={bookmarks.length} />
-          <Card title="Medium Posts" count={10} />
-          <Card title="Dev.to Posts" count={15} />
-          <Card title="Hashnode Posts" count={5} />
-          <Card title="Total Posts" count={30} />
-        </CardsContainer>
-      </MainContent>
+      <Sidebar
+        menuOpen={menuOpen}
+        setMenuOpen={setMenuOpen}
+        setActiveComponent={setActiveComponent}
+      />
+      <MainContent>{renderContent()}</MainContent>
     </DashboardContainer>
   );
 };
@@ -117,6 +145,8 @@ const DashboardContainer = styled.div`
 
 const MainContent = styled.div`
   flex-grow: 1;
+  flex-basis: 100%;
+  min-height: 100vh;
   padding: 20px;
   background-color: #f0f2f5;
 

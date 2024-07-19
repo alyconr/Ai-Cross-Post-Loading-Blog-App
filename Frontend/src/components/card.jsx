b9d7fcafda1e3cedae0026a2642147f7ea36516f
alyconr <alyconr@hotmail.com>
2024-07-19 15:31:53 -0500

    :fire: Remove  unneccesary code

--- Frontend/src/components/card.jsx
+++ Frontend/src/components/card.jsx
@@ -1,33 +1,26 @@
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const Card = ({ title, count }) => {
-    return (
-      <CardContainer>
-        <Title>{title}</Title>
-        <Count>{count}</Count>
-      </CardContainer>
-    );
-  };
-  
-export default Card;
-  
 const CardContainer = styled.div`
-  background: white;
-  border-radius: 10px;
+  background-color: white;
+  border-radius: 8px;
   padding: 20px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  width: auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
 `;
 
-const Title = styled.h2`
-  margin: 0 0 10px;
+const CardTitle = styled.h2`
+  margin-top: 0;
+  margin-bottom: 15px;
 `;
 
-const Count = styled.p`
-  font-size: 24px;
-  font-weight: bold;
-  margin: 0;
-`;
+const Card = ({ title, children, count }) => (
+  <CardContainer>
+    <CardTitle>
+      {title} {count !== undefined && `(${count})`}
+    </CardTitle>
+
+    {children}
+  </CardContainer>
+);
+
+export default Card;

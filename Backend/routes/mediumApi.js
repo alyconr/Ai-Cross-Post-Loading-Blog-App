f299121fee537bf187f1c8119241d2e543e6802e
alyconr <alyconr@hotmail.com>
2024-07-22 10:39:09 -0500

    :heavy_plus_sign: Add  routes to handle medium api endpoints

--- Backend/routes/mediumApi.js
+++ Backend/routes/mediumApi.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { postMediumApi } = require("../controllers/mediumApi");
+const { postMediumApi, getMediumPosts } = require("../controllers/mediumApi");
 
 router.route("/").post(postMediumApi);
+router.route("/:userId").get(getMediumPosts);
 
 module.exports = router;

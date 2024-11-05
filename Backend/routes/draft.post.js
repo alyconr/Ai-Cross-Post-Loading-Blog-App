c05e8c5bfdc2b37cdba57793f80be3d628936903
Aly <alyconr@hotmail.com>
2024-11-05 11:34:14 -0500

    :sparkles: New feature to implementy a delete draft by id

--- Backend/routes/draft.post.js
+++ Backend/routes/draft.post.js
@@ -7,13 +7,18 @@ const {
   updateDraftPost,
   getSingleDraftPost,
   deleteDraftPost,
+  deleteDraftPostById
 } = require("../controllers/draft.post");
+
+const { apiAuthorized} = require("../middleware/authorizedUser");
 
 router
   .route("/")
   .get(getAllDraftPosts)
   .post(createDraftPost)
   .delete(deleteDraftPost);
+
+router.route("/:id").delete(apiAuthorized, deleteDraftPostById);
 router.route("/:id").put(updateDraftPost).get(getSingleDraftPost);
 
 module.exports = router;

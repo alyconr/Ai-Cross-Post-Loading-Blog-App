9d8ee297b26c7e4a69ef395afddbc2cf36303dc1
Aly <alyconr@hotmail.com>
2024-11-11 21:41:43 -0500

    :sparkles: Refactor vite conf

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default ({ mode }) => {
+  const env = loadEnv(mode, '.');
   
-})
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api/v1': {
+          target: env.VITE_API_URI,
+          changeOrigin: true,
+          secure: false,
+        }
+      },
+      port: 5173,
+      host: true
+    },
+    build: {
+      outDir: 'dist',
+      sourcemap: true
+    }
+  });
+};
